e2cf6150971023705291bfd7091189d554a2d1dc
Loyer Luo <loyer-yuan@qq.com>
2025-05-13 13:42:48 +0800

    [refactor] Refactor the input.

--- src/cpu/test_all.cpp
+++ src/cpu/test_all.cpp
@@ -406,7 +406,7 @@ int main(int argc, char *argv[])
         std::cout << "End of test!" << std::endl;
         std::cout << "----------------------------------------" << std::endl;
     }
-
+#ifdef TESTALL
     //
     // Test COO kernel
     //
@@ -484,6 +484,7 @@ int main(int argc, char *argv[])
         std::cout << "End of test!" << std::endl;
         std::cout << "----------------------------------------" << std::endl;
     }
+#endif
 
     return 0;
 }
